03dd43661034db86c53b1c4897fe002c333b1f53
Sara Jensen <sara.jensen@idealist.org>
2016-08-12 00:57:00 -0700

    start javascript logic, store first two variable, add comments to code to explain the purpose of functions, listeners and calls.

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,16 @@ $(document).ready(function() {
   $("form").submit(function(event) {
   event.preventDefault();
 
+  companyType = 0;
+  appType = 0;
 
-
-
-  var companyType = parseInt($("input:radio[name=companyType]:checked").val());
+  var companyType = parseInt($("input:radio[name=orgType]:checked").val());
   alert(companyType);
+  var appType = parseInt($("input:radio[name=appType]:checked").val());
+  alert(appType);
 
+
+  if
 
 
   });
